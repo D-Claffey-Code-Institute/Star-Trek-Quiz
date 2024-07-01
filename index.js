77a744dd8cdd825e58be3a2cc795f936f71103a6
D-Claffey-Code-Institute <d7x359i6@students.codeinstitute.net>
2024-07-01 23:41:12 +0000

    add check answer and show answer function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /* declaring variables for later use */
-let username;
-let questionCounter;
-let answers;
-let correctAnswers;
-let incorrectAnswers;
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('result');
+const submitButton = document.getElementById('submit');
+const retryButton = document.getElementById('retry');
+const showAnswerButton = document.getElementById("showAnswer");
+
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = [];
 
 /* Questions and Answers*/
 
@@ -15,7 +19,7 @@ const quiz = [
     },
     {
         question: "Who wrote the 'The Theme From Star Trek'?",
-        choice1: ["Gene Roddenberry", "John Williams", "Alexander Courage", "William Shatner"], 
+        choice1: ["Gene Roddenberry", "John Williams", "Alexander Courage", "William Shatner"],
         answer: "Alexander Courage"
     },
     {
@@ -59,3 +63,107 @@ const quiz = [
         answer: "Majel Barrett",
     },
 ];
+
+//Shuffle quetions 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+
+    const questionElement = document.createElement("div");
+    questionElement.className = "question";
+    questionElement.innerHTML = questionData.question;
+
+    const optionsElement = document.createElement("div");
+    optionsElement.classname = "options";
+
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+
+    for (let i = 0; i < shuffledOptions.length; i++) {
+        const option = document.createElement("label");
+        option.className = "option";
+
+        const radio = document.getElementById("input");
+        radio.type = "radio";
+        radio.name = "quiz";
+        radio.value = shuffledOptions[i];
+
+        const optionText = document.createTextNode(shuffledOptions[i]);
+        option.appendChild(radio);
+        option.appendChild(optionText);
+        optionsElement.appendChild(option);
+
+    }
+
+    quizContainer.innerHTML = "";
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+
+}
+
+function checkAnswer() {
+    const selectedOption = document.querySelector("input[name='quiz']:checked");
+    if (selectedOption) {
+        const answer = selectedOption.value;
+        if (answer === quizData[currentQuestion].answer) {
+            score++;
+
+        } else {
+            incorrectAnswers.push({
+                question: quizData[currentQuestion].question,
+                incorrectAnswer: answer,
+                correctAnswer: quizData[currentQuestion].answer,
+            });
+        }
+
+        currentQuestion++;
+        selectedOption.checked = false;
+        if (currentQuestion < quizData.lenght) {
+            displayQuestions();
+
+        } else {
+            displayResult();
+        }
+    }
+}
+
+function showAnswer() {
+    quizContainer.style.display = "none";
+    submitButton.style.display = "none";
+    retryButton.style.display = "none";
+    showAnswerButton.style.display = "none";
+
+    let incorrectAnswersHtml= "";
+    for (i = 0; i < incorrectAnswers.lenght; i++) {
+        incorrectAnswersHtml += 
+        `
+        <p>
+        <strong>Question:</strong> ${incorrectAnswers[i].question}
+        <br>
+        <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}
+        <br>
+        <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+        </p>
+        `;
+
+    }
+
+    resultContainer.innerHTML = 
+    `
+    <p>You Scored ${score} out of 10!</p>
+    <p>Incorrect Answers:</p>
+    ${incorrectAnswersHtml}
+    `;
+}
+
+submitButton.addEventListener("click", checkAnswer);
+retryButton.addEventListener("click", retryQuiz);
+showAnswerButton.addEventListener("click", showAnswer);
+
+displayQuestion();
